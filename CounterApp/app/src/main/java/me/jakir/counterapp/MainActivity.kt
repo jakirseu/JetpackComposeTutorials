2503fdf1bdbfb379a8b5ff8d191f142ad94e1e53
Jakir Hossain <jakirseu@gmail.com>
2024-11-14 19:12:19 +0600

    mutableIntStateOf change

--- CounterApp/app/src/main/java/me/jakir/counterapp/MainActivity.kt
+++ CounterApp/app/src/main/java/me/jakir/counterapp/MainActivity.kt
@@ -15,6 +15,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -37,7 +38,7 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Counter(modifier: Modifier = Modifier) {
-    var count by remember { mutableStateOf(0) }
+    var count by remember { mutableIntStateOf(0) }
 
     Column(modifier = modifier.fillMaxSize(),
         horizontalAlignment = Alignment.CenterHorizontally,
